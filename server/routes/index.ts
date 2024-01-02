5081d3c619f75f9e06d4484e7c35a0ea59e92aae
Brahm Van Houzen <brahmvanh@gmail.com>
2024-01-02 01:14:57 -0500

    router was serving up the wrong response

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import apiRoutes from './api';
+import path from 'path';
 
 const router = Router();
 
 router.use('/api', apiRoutes);
 
-router.use((req: Request, res: Response, next: NextFunction) => {
-	res.send('Wrong route!');
+router.use((req: Request, res: Response) => {
+	res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 export default router;
