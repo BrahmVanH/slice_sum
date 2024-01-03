68f0f8b690d5de945bf947aa43e9734596bfb77d
Brahm Van Houzen <brahmvanh@gmail.com>
2024-01-02 21:58:52 -0500

    moved use(routes)

--- server/server.ts
+++ server/server.ts
@@ -17,12 +17,16 @@ dotenv.config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+
 app.use(authMiddleware);
+
 app.use(cors(corsOptions));
+
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build/static')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.use(routes);
 
 db.once('open', () => {
 	app.listen(PORT, () => {
