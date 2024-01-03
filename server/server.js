feb5af709b789ba569cc8998eac8c139a20aaf32
Brahm Van Houzen <brahmvanh@gmail.com>
2024-01-02 21:28:32 -0500

    updated cors policy and add logic to server up the client from the server in prod... whoops

--- server/server.js
+++ server/server.js
@@ -9,10 +9,12 @@ const connection_1 = require("./config/connection");
 const routes_1 = __importDefault(require("./routes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const auth_1 = require("./utils/auth");
+const path_1 = __importDefault(require("path"));
 const PORT = process.env.PORT || 3001;
 const app = (0, express_1.default)();
-var corsOptions = {
-    origin: 'http://localhost:3000',
+const allowedOrigins = ['https://slicesum.org', 'https://slice-sum-25ec1c3c5361.herokuapp.com'];
+const corsOptions = {
+    origin: process.env.NODE_ENV === 'production' ? allowedOrigins : 'http://localhost:3000',
     optionsSuccessStatus: 200,
 };
 dotenv_1.default.config();
@@ -21,6 +23,9 @@ app.use(express_1.default.json());
 app.use(routes_1.default);
 app.use(auth_1.authMiddleware);
 app.use((0, cors_1.default)(corsOptions));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express_1.default.static(path_1.default.join(__dirname, '../client/build')));
+}
 connection_1.db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}`);
--- server/server.ts
+++ server/server.ts
@@ -4,11 +4,13 @@ import { db } from './config/connection';
 import routes from './routes';
 import dotenv from 'dotenv';
 import { authMiddleware } from './utils/auth';
+import path from 'path';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-var corsOptions = {
-	origin: 'http://localhost:3000',
+const allowedOrigins = ['https://slicesum.org', 'https://slice-sum-25ec1c3c5361.herokuapp.com'];
+const corsOptions = {
+	origin: process.env.NODE_ENV === 'production' ? allowedOrigins : 'http://localhost:3000',
 	optionsSuccessStatus: 200,
 };
 dotenv.config();
@@ -18,10 +20,12 @@ app.use(express.json());
 app.use(routes);
 app.use(authMiddleware);
 app.use(cors(corsOptions));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 db.once('open', () => {
 	app.listen(PORT, () => {
 		console.log(`API server running on port ${PORT}`);
-
 	});
 });
