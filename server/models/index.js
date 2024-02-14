fe03a49955134ee8f902b2e6760f02d6309aef5e
Brahm Van Houzen <brahmvanh@gmail.com>
2024-02-13 20:18:37 -0500

    added build script since netify doesnt support t-node

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SliceEntry = exports.User = void 0;
-const User_1 = require("./User");
-Object.defineProperty(exports, "User", { enumerable: true, get: function () { return User_1.User; } });
-const SliceEntry_1 = require("./SliceEntry");
-Object.defineProperty(exports, "SliceEntry", { enumerable: true, get: function () { return SliceEntry_1.SliceEntry; } });
