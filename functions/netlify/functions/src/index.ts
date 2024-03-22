308d6aafabb59db97505a27f5a3798906ecd40a6
Brahm Van Houzen <brahmvanh@gmail.com>
2024-03-21 23:51:34 -0400

    added uncommented server middleware

--- functions/netlify/functions/src/index.ts
+++ functions/netlify/functions/src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-//  { Request, Response, NextFunction } 
+import express, { Request, Response, NextFunction } from 'express';
+//  { Request, Response, NextFunction }
 import serverless from 'serverless-http';
 import dotenv from 'dotenv';
 import router from './routes';
@@ -8,25 +8,25 @@ dotenv.config();
 
 const app = express();
 
-// interface IRequest extends Request {
-// 	headers: {
-// 		host?: string;
-// 	};
-// }
+interface IRequest extends Request {
+	headers: {
+		host?: string;
+	};
+}
 
-// const getAllowedOrigins = (req: IRequest, res: Response, next: NextFunction) => {
-// 	const allowedOrigins = ['https://slicesum.org', 'https://slice-sum-25ec1c3c5361.herokuapp.com'];
+const getAllowedOrigins = (req: IRequest, res: Response, next: NextFunction) => {
+	const allowedOrigins = ['https://main--slice-sum.netlify.app/', 'http://localhost:3000', 'https://slice-sum.netlify.app/'];
 
-// 	const host = req.headers.host ?? '';
-// 	console.log('host', host);
+	const host = req.headers.host ?? '';
+	console.log('host', host);
 
-// 	if (allowedOrigins.includes(host)) {
-// 		next();
-// 	} else {
-// 		res.status(405).send('Host not allowed');
-// 	}
-// };
-// app.use(getAllowedOrigins);
+	if (allowedOrigins.includes(host)) {
+		next();
+	} else {
+		res.status(405).send('Host not allowed');
+	}
+};
+app.use(getAllowedOrigins);
 
 app.use('/.netlify/functions/actions', router);
 
